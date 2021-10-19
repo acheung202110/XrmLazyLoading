19afd5e1a8e2957d0118c64d4ac4d79b8c1a36c8
ACheung6102 <ACheung6102@HKHKG-AXC6102X1>
2021-10-19 15:46:47 +0800

    Add project files.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Zap Technology Pty Ltd")]
+[assembly: AssemblyCopyright("Copyright © Zap Technology Pty. Ltd. 2017")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
